ed3acd41ffc7b98667750a8cd5c60fe9e7bf9efd
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-05-20 17:54:00 -0400

    cc_2.js
    "Test accelerate and brake methods on Car instances."

--- cc_2.js
+++ cc_2.js
@@ -20,3 +20,14 @@ brake() {
 }
 
 }
+
+// Task 4: Create and Test Car Objects
+const car1 = new Car(`BMW`, 120);
+const car2 = new Car(`Mercedes`, 95);
+
+// Test accelerate and brake methods on Car instances
+car1.accelerate(); // Output: BMW is going at 130 km/h
+car1.brake(); // Output: BMW is going at 125 km/h
+
+car2.accelerate(); // Output: Mercedes is going at 105 km/h
+car2.brake(); // Output: Mercedes is going at 100 km/h
